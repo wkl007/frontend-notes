17765e8f2b497eb6e275fa418e41c41f536c1e32
wkl--007 <499657357@qq.com>
2019-03-08 21:48:09 +0800

    3.8 update webpack

---
+++ Webpack/lesson1/header.js
@@ -0,0 +1,8 @@
+function Header() {
+	var dom = document.getElementById('root');
+	var header = document.createElement('div');
+	header.innerText = 'header';
+	dom.append(header);
+}
+
+export default Header
